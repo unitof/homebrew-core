d835515b774bf548ccc27037e534257e31171e5e
Thomas Bachem <mail@thomasbachem.com>
2022-04-16 00:30:23 +0200

    video-compare 20220412 (new formula)

---
+++ Formula/video-compare.rb
@@ -0,0 +1,21 @@
+class VideoCompare < Formula
+  desc "Split screen video comparison tool using FFmpeg and SDL2"
+  homepage "https://github.com/pixop/video-compare"
+  url "https://github.com/pixop/video-compare/archive/refs/tags/20220412.tar.gz"
+  sha256 "425eafd0095e8043c240658618c14c8a4e92954c41cd1adf5a3b01d275a3581c"
+  license "GPL-2.0-only"
+
+  depends_on "ffmpeg"
+  depends_on "sdl2"
+  depends_on "sdl2_ttf"
+
+  def install
+    system "make"
+    bin.install "video-compare"
+  end
+
+  test do
+    output = shell_output("#{bin}/video-compare 2>&1")
+    assert_match "Usage: ", output
+  end
+end
