2e5800e8ae94b9a483176774b5d81aa826be508a
Jacob Ford <j@cobford.com>
2023-02-23 01:43:36 -0500

    imessage-exporter (new formula)

---
+++ Formula/imessage-exporter.rb
@@ -0,0 +1,22 @@
+class ImessageExporter < Formula
+  desc "iMessage conversation database export and inspector"
+  homepage "https://github.com/ReagentX/imessage-exporter"
+  url "https://github.com/ReagentX/imessage-exporter/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "b932862b5f4c1c607c4dd401f99d088b4e1291ce7d5c2a7d1c40da9c91478e97"
+  license "GPL-3"
+
+  depends_on "rust" => :build
+
+  def install
+    # manifest set to 0.0.0 for some reason, matching upstream build behavior
+    # https://github.com/ReagentX/imessage-exporter/blob/develop/build.sh
+    inreplace "imessage-exporter/Cargo.toml", "version = \"0.0.0\"",
+                                              "version = \"#{version.to_s}\""
+    system "cargo", "install", *std_cargo_args(path: "imessage-exporter")
+  end
+
+  test do
+    assert_match version.to_s, shell_output(bin/"imessage-exporter --version")
+    assert_match "Unable to launch", shell_output(bin/"imessage-exporter --diagnostics")
+  end
+end
