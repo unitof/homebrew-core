85df57a9eb3324250ddff1cf91eee4cf606bde2b
Jacob Ford <j@cobford.com>
2024-03-17 15:54:24 -0400

    manim: fix build error with pyglet resource
    See https://github.com/pyglet/pyglet/issues/999
    
    Approach borrowed from iredis.rb

--- Formula/m/manim.rb
+++ Formula/m/manim.rb
@@ -185,7 +185,17 @@ class Manim < Formula
   end
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.12")
+
+    # pyglet@1.5.28 contains error in pyproject.toml, causing build error: `TypeError: expected str, bytes or os.PathLike object, not list`
+    # See https://github.com/pyglet/pyglet/issues/999
+    resource("pyglet").stage do
+      inreplace "pyproject.toml", 'build-backend = ["flit_core.buildapi", "flit_core.wheel"]', 'build-backend = "flit_core.buildapi"'
+      venv.pip_install_and_link Pathname.pwd
+    end
+
+    venv.pip_install resources.reject { |r| r.name == "pyglet" }
+    venv.pip_install_and_link buildpath
   end
 
   test do
