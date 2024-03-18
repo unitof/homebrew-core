d04d80e5bc344b53c55046b450181f4894910eba
Jacob Ford <j@cobford.com>
2024-03-18 18:40:49 -0400

    manim: correct way to reject pyobjc on linux
    Stolen from gimme-aws-creds

--- Formula/m/manim.rb
+++ Formula/m/manim.rb
@@ -16,18 +16,6 @@ class Manim < Formula
   depends_on "py3cairo"
   depends_on "python@3.12"
 
-  on_macos do
-    resource "pyobjc-core" do
-      url "https://files.pythonhosted.org/packages/24/ac/61c58e65780c6ba0523997d236fac99e38e5ddfabfd5b500409f8239a257/pyobjc-core-10.2.tar.gz"
-      sha256 "0153206e15d0e0d7abd53ee8a7fbaf5606602a032e177a028fc8589516a8771c"
-    end
-
-    resource "pyobjc-framework-cocoa" do
-      url "https://files.pythonhosted.org/packages/b0/c0/7eb30628e1a60c8b700f0b15280417c754eda9f186d05d47f4cac6f4e1a7/pyobjc-framework-Cocoa-10.2.tar.gz"
-      sha256 "6383141379636b13855dca1b39c032752862b829f93a49d7ddb35046abfdc035"
-    end
-  end
-
   on_linux do
     depends_on "cmake" => :build
     depends_on "patchelf" => :build
@@ -36,6 +24,16 @@ class Manim < Formula
   on_arm do
     depends_on "pango"
     depends_on "scipy"
+  end
+
+  resource "pyobjc-core" do
+    url "https://files.pythonhosted.org/packages/24/ac/61c58e65780c6ba0523997d236fac99e38e5ddfabfd5b500409f8239a257/pyobjc-core-10.2.tar.gz"
+    sha256 "0153206e15d0e0d7abd53ee8a7fbaf5606602a032e177a028fc8589516a8771c"
+  end
+
+  resource "pyobjc-framework-cocoa" do
+    url "https://files.pythonhosted.org/packages/b0/c0/7eb30628e1a60c8b700f0b15280417c754eda9f186d05d47f4cac6f4e1a7/pyobjc-framework-Cocoa-10.2.tar.gz"
+    sha256 "6383141379636b13855dca1b39c032752862b829f93a49d7ddb35046abfdc035"
   end
 
   resource "certifi" do
@@ -229,7 +227,9 @@ class Manim < Formula
   end
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resources.reject { |r| r.name.start_with?("pyobjc") && OS.linux? }
+    venv.pip_install_and_link buildpath
   end
 
   test do
